efee86fdd0094dbd90bd77c10991f469b8a957e8
alejandrodk <ottoabarriosp@hotmail.com>
2020-05-30 15:30:45 -0300

    define controllers

--- controllers/consultas.js
+++ controllers/consultas.js
@@ -1,5 +1,19 @@
+const db = require("../database/models");
+
 module.exports = {
     consultas : (req, res) => {
         
+    },
+    detail : (req, res) => {
+
+    },
+    create : (req, res) => {
+
+    },
+    update : (req, res) => {
+
+    },
+    delete : (req, res) => {
+
     }
 }
