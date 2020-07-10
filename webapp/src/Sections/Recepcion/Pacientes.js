5de96ad2bb376e544b9f798fd01210f8b259c94b
alejandrodk <ottoabarriosp@hotmail.com>
2020-07-09 21:53:46 -0300

    Pacients list and filter Component

---
+++ webapp/src/Sections/Recepcion/Pacientes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SearchPacients from '../../components/SearchPacients/';
+
+const Pacientes = () => {
+    /* 
+    Tuve que crear un nuevo componente y traer todo junto, ya que
+    este componente no me permite utilizar estados
+    */
+    return (
+        <SearchPacients/>
+    )
+}
+
+export default Pacientes
